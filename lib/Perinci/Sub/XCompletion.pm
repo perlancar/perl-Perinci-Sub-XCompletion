eedc03a988b8de2c1977ef6182c26b0816488179
perlancar (@pc-home) <perlancar@gmail.com>
2017-01-15 09:15:48 +0700

    Mention x.index_completion; x.completion et al can also be a coderef like Rinci argument spec's 'completion' property

--- lib/Perinci/Sub/XCompletion.pm
+++ lib/Perinci/Sub/XCompletion.pm
@@ -4,7 +4,7 @@ package Perinci::Sub::XCompletion;
 # VERSION
 
 1;
-# ABSTRACT: Write completion routines in x.{element_,}completion attributes
+# ABSTRACT: Write completion routines in x.{,element_,index_}completion attributes
 
 =head1 SYNOPSIS
 
@@ -24,9 +24,12 @@ In L<Rinci> metadata:
 
 This is a module to support an experimental (and temporary?) way to ease writing
 completion routine. Instead of putting a coderef in C<completion> or
-C<element_completion> property in argument specification, you put C<< [$name,
-\%args] >> in C<x.completion> or C<x.element_completion> attributes. C<$name> is
-the name of submodule to use, and \%args is arguments.
+C<element_completion> property in argument specification, you put C<$name> or
+C<< [$name, \%args] >> in C<x.completion> or C<x.element_completion> or
+C<x.index_completion> attributes. C<$name> is the name of
+C<Perinci::Sub::XCompletion::*> submodule to use, and \%args is arguments.
+
+But you can also use a coderef as usual.
 
 
 =head1 SEE ALSO
